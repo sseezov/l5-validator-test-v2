1e552ba4f50490ba3f30bf78f6fcdaaa3210e0d4
sseezov <sseezov@mail.ru>
2023-10-17 18:08:36 +0700

    update

--- src/ArraySchema.js
+++
@@ -1,7 +0,0 @@
-export default class ArraySchema {
-  validators = [(value) => Array.isArray(value)];
-
-  isValid(array) {
-    return this.validators.every((validator) => validator(array));
-  }
-}
--- src/Validator.js
+++ src/Validator.js
@@ -1,11 +1,1 @@
-import ArraySchema from './ArraySchema.js';
-import StringSchema from './StringSchema.js';
 
-export default class Validator {
-  string() {
-    return new StringSchema();
-  }
-  array() {
-    return new ArraySchema();
-  }
-}
