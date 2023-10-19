39a5bc94ec89a7be1b8052129c6cb9e47c1d7ec5
sseezov <sseezov@mail.ru>
2023-10-19 19:52:48 +0700

    raw solution

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,7 +1,25 @@
+import _ from 'lodash';
+
 export default class ArraySchema {
   validators = [(value) => Array.isArray(value)];
 
   isValid(array) {
     return this.validators.every((validator) => validator(array));
+  }
+
+  maxDepth(max) {
+    const validator = (values, depth = -1) => {
+      if (!Array.isArray(values)) {
+        console.log(7777777, depth, max, depth > max);
+        return depth;
+      }
+
+      const result = values.map((value) => validator(value, depth + 1));
+      return _.flattenDeep(result);
+    };
+
+    const v = (value) => validator(value).every((val) => val <= max);
+    this.validators.push(v);
+    return this;
   }
 }
--- src/Validator.js
+++ src/Validator.js
@@ -1,11 +1,21 @@
 import ArraySchema from './ArraySchema.js';
+import ObjectSchema from './ObjectSchema.js';
 import StringSchema from './StringSchema.js';
 
 export default class Validator {
   string() {
     return new StringSchema();
   }
+
   array() {
     return new ArraySchema();
   }
+
+  object() {
+    return new ObjectSchema();
+  }
 }
+
+const validator = new Validator();
+const schema1 = validator.array().maxDepth(3);
+console.log(schema1.isValid([0, 0, 0, 0, [1, [2]]]));
